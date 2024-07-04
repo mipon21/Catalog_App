bdd35e133639248cf4a4465e1d4bba81204702c4
mipon21 <mipon5500u@gmail.com>
2024-07-04 22:18:54 +0600

    Day 04 done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:app04/pages/home_page.dart';
 import 'package:app04/pages/login_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,9 +18,12 @@ class MyApp extends StatelessWidget {
 
 
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       debugShowCheckedModeBanner: true,
      // home: HomePage(),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/" : (context)=> LoginPage(),
         "/home" : (context) =>HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,11 +6,47 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page", style: TextStyle(
-          fontSize: 40,
-          color: Colors.blue,
-        ),),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+          ),
+          SizedBox(height: 20,),
+          Text('WelCome', style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter User Name",
+                    label: Text("User Name"),
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    label: Text("Password"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed: () {
+            print("Hi There");
+          },
+            child: Text("Continue"),
+            style: TextButton.styleFrom(),
+          )
+        ],
       ),
     );
   }
