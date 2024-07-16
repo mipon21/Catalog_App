ad8a33c3305b6d8a2d7c7fc554f4fe7e19e63262
mipon21 <mipon5500u@gmail.com>
2024-07-16 23:21:41 +0600

    Day 22 done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+// ignore_for_file: prefer_const_constructors
+
 import 'package:app04/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -18,7 +22,69 @@ class CartPage extends StatelessWidget {
         centerTitle: true,
       ),
       backgroundColor: context.canvasColor,
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
 
     );
   }
 }
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$999".text.xl4.color(context.theme.primaryColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: "Buy".text.xl2.color(Colors.white).make(),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  context.theme.floatingActionButtonTheme.backgroundColor,
+                ),
+                shape: MaterialStateProperty.all(
+                    StadiumBorder()
+                )
+            ),
+          ).wh(100, 50)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline),
+            onPressed: (){},
+          ),
+          title: "Item 1".text.make(),
+        ),
+        itemCount: 5,
+    );
+  }
+}
+
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:app04/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -33,6 +34,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.canvasColor,
       body: SingleChildScrollView(
         child: Column(
           children: [
